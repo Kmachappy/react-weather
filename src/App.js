b9ebe7d800f59c99da82500d15c58d5638bac21e
Alejandro Rojas <kmachappy@pm.me>
2022-04-30 17:05:45 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./styles.css";
+import weatherData from "./weatherData";
+import WeatherForecast from "./components/WeatherForecast";
 
 export default function App() {
+  // console.log(weatherData)
+  const allWeatherData = weatherData.map((element,index)=>{
+    return  <WeatherForecast img={element.img} condition={element.conditions} time={element.time} key={index} />
+
+  })
+  
+  
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <h1>weatherData</h1> 
+      <section>
+        {allWeatherData}
+      </section>
     </div>
   );
 }
---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,14 @@
+import React from "react"
+const WeatherForecast =(props)=>{
+    return(
+        
+        <div className="weather">
+        <img src={props.img} alt=""/>
+        <p><span>conditions:</span> {props.condition}</p>
+        <p><span>time:</span> {props.time}</p>
+        </div>
+    )
+}
+
+
+export default WeatherForecast
